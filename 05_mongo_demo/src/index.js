283cb2834f4cfcd9821811fd494a0527b6dd2778
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-07-18 21:16:41 -0500

    Playing with crud on mongo

--- 05_mongo_demo/src/index.js
+++ 05_mongo_demo/src/index.js
@@ -31,17 +31,47 @@ async function createCourse() {
 //createCourse();
 
 async function getCourses() {
-    // const courses = await Course.find({ author: 'Mosh', isPublished: true})
-    //const courses = await Course.find({ price: {$gte: 10}}) // things with a price greather than 10
-    // const courses = await Course.find({ price: {$in: [10, 25]}}) // things with a price greather than 10
-     const courses = await Course.find()
-         .or([ {author: 'Mosh'}, {isPublished: true}])
+    const courses = await Course.find({ author: 'Mosh', isPublished: true})
         .limit(10)
         .sort({ name:1 })
-        .select({name: 1, tages: 1});
-    console.log('Courses', courses);
+        .select({name: 1, tages: 1, author:1});
+
+    const cnt =  await Course.find({ author: 'Mosh', isPublished: true})
+        .limit(10)
+        .sort({ name:1 })
+        .count();
+    console.log('Courses: ', courses);
+    console.log('Total Courses: ', cnt)
 }
 
-getCourses();
+ getCourses();
 
+
+async function updateCourse(id) {
+    // Query first
+    const course = await Course.findById(id);
+    if(!course){
+        console.log('not found');
+        return;
+    } 
+
+    course.set({isPublished: false, author: 'Other Author'});
+    const result = await course.save();
+    console.log('result: ', result);
+}
+
+async function updateCourseWQ(id) {
+    const course = await Course.update({_id: id }, {$set: {isPublished: true}});
+    console.log('Result second update: ', course);
+
+}
+
+
+updateCourse('5d2e82aadeb1c959343dbdb3');
+updateCourseWQ('5d2e82aadeb1c959343dbdb3');
+
+async function removeCourse(id) {
+    const result = await Course.deleteOne({ _id: id});
+    console.log('Deleted course: ', result);
+}
  
