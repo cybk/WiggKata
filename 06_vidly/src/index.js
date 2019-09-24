c00414ed45d9aba7970204bede39d655b787d164
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-09-23 22:11:37 -0500

    Added genres persistance on Mongodb

--- 06_vidly/src/index.js
+++ 06_vidly/src/index.js
@@ -1,7 +1,15 @@
-import Joi from 'joi';
+
 import genres from './routes/genres';
 import express, { json } from 'express';
+import mongoose from 'mongoose';
+import config from 'config';
+
 const app = express();
+
+mongoose.connect(config.get('mongoUri'), { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to Mongodb....'))
+    .catch(err => console.log('Could not connect to mongodb. . .', err));
+
 
 app.use(json());
 app.use('/api/genres', genres);
